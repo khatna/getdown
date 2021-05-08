e7b8bd245a07a4c8f483545703c95b9674aa0dab
Zoe Kahana <zkahana@princeton.edu>
2021-05-08 10:41:10 -0400

    outline platforms

--- src/components/objects/Platform/Platform.js
+++ src/components/objects/Platform/Platform.js
@@ -3,11 +3,44 @@ import {
     BoxGeometry,
     MeshToonMaterial,
     Mesh,
-    DoubleSide
+    Vector3,
+    ShaderMaterial,
+    BackSide,
+    FrontSide,
+    MeshPhongMaterial
 } from 'three';
 
 const PLATFORM_MAIN_LENGTH = 6;
 const PLATFORM_THICKNESS = 0.5;
+
+// SHADERS ADAPTED FROM EXAMPLE: https://discourse.threejs.org/t/how-to-render-geometry-edges/5745
+
+const vertexShader = `
+varying vec3 vPos;
+void main()	{
+  vPos = position;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+}
+`
+
+const fragmentShader = `
+varying vec3 vPos;
+uniform vec3 size;
+uniform float thickness;
+uniform float smoothness;
+uniform vec3 color;
+
+void main() {
+        
+    float a = smoothstep(thickness, thickness + smoothness, length(abs(vPos.xy) - size.xy));
+    a *= smoothstep(thickness, thickness + smoothness, length(abs(vPos.yz) - size.yz));
+    a *= smoothstep(thickness, thickness + smoothness, length(abs(vPos.xz) - size.xz));
+    
+    vec3 c = mix(vec3(0), color, a);
+    
+    gl_FragColor = vec4(c, 1.0);
+}
+`
 
 class Platform extends Group {
     constructor() {
@@ -20,9 +53,44 @@ class Platform extends Group {
             PLATFORM_THICKNESS,
             PLATFORM_MAIN_LENGTH
         );
-        let platformMaterial = new MeshToonMaterial({color: 0x333333, side: DoubleSide});
+        // MATERIAL ADAPTED FROM EXAMPLE: 
+        /*
+        let platformMaterial = new ShaderMaterial({
+            uniforms: {
+                size: {
+                    value: new Vector3(
+                        platformMainGeometry.parameters.width, 
+                        platformMainGeometry.parameters.height, 
+                        platformMainGeometry.parameters.depth
+                    ).multiplyScalar(0.5)
+                },
+                thickness: {
+                    value: 0.05
+                },
+                smoothness: {
+                    value: 0
+                },
+                color: {
+                    value: new Vector3(0.2, 0.2, 0.2)
+                }
+            },
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader
+          });
+        this.add(new Mesh(platformMainGeometry, platformMaterial));*/
 
-        this.add(new Mesh(platformMainGeometry, platformMaterial));
+        let platformMaterial = new MeshToonMaterial({color: 0x000000, side: BackSide});
+        let outlineMaterial = new MeshPhongMaterial({color:0x333333, side: FrontSide});
+
+        let group = new Group;
+        
+        let platform = new Mesh(platformMainGeometry, platformMaterial)
+        const s = 1.03
+        platform.scale.set(s,s,s)
+        group.add(platform);
+        let outline = new Mesh(platformMainGeometry, outlineMaterial);
+        group.add(outline);
+        this.add(group)
 
     }
 }
