97f9e5956a187b6cbaced96b82b7c888a533b6c7
Willie Chang <williehwc@gmail.com>
2021-05-09 11:41:49 -0400

    Platform collapse; change platform color

--- src/components/objects/Platform/Platform.js
+++ src/components/objects/Platform/Platform.js
@@ -110,21 +110,78 @@ class Platform extends Group {
         //   });
         // this.add(new Mesh(platformMainGeometry, platformMaterial));
 
-        let platformMaterial = new MeshStandardMaterial({color: 0x8AC926, side: DoubleSide});
-        let platformCenterMaterial = new MeshStandardMaterial({color: 0xffffff, side: FrontSide});
-        let outlineMaterial = new MeshStandardMaterial({color:0x000000, side: BackSide});
+        this.platformMaterial = new MeshStandardMaterial({color: 0xFFCA3A, side: DoubleSide});
+        this.platformCenterMaterial = new MeshStandardMaterial({color: 0xffffff, side: FrontSide});
+        this.outlineMaterial = new MeshStandardMaterial({color:0x000000, side: BackSide});
 
         let group = new Group;
         
-        let platform = new Mesh(platformMainGeometry, platformMaterial)
+        let platform = new Mesh(platformMainGeometry, this.platformMaterial)
         platform.scale.multiplyScalar(0.95);
         group.add(platform);
-        let center = new Mesh(platformCenterGeometry, platformCenterMaterial);
+        let center = new Mesh(platformCenterGeometry, this.platformCenterMaterial);
         group.add(center);
-        let outline = new Mesh(platformMainGeometry, outlineMaterial);
+        let outline = new Mesh(platformMainGeometry, this.outlineMaterial);
         group.add(outline);
+
         this.add(group);
 
+        this.collapsing = false;
+    }
+
+    collapse(timeStamp) {
+        if (!this.collapsing) {
+            this.remove.apply(this, this.children);
+            let shape = drawRoundedSquare(0, 2);
+            let geometry = new ExtrudeBufferGeometry(shape, {
+                depth: 3,
+                bevelEnabled: true,
+                bevelSegments: 4,
+                steps: 1,
+                bevelSize: 1,
+                bevelThickness: 1,
+                curveSegments: 0
+            });
+            this.pieces = [];
+            this.fallSpeed = [];
+            this.rotateSpeedX = [];
+            this.rotateSpeedY = [];
+            this.rotateSpeedZ = [];
+            for (let x = -1; x <= 1; x++) {
+                for (let z = -1; z <= 1; z++) {
+                    let piece = new Mesh(
+                        geometry,
+                        Math.random() > 0.5
+                            ? this.platformMaterial
+                            : this.platformCenterMaterial
+                    );
+                    piece.position.x = x * 3;
+                    piece.position.z = z * 3;
+                    piece.rotateX(Math.random());
+                    piece.rotateY(Math.random());
+                    piece.rotateZ(Math.random());
+                    piece.scale.multiplyScalar(0.5);
+                    piece.material.transparent = true;
+                    this.pieces.push(piece);
+                    this.fallSpeed.push(0.2 + Math.random());
+                    this.rotateSpeedX.push(Math.random() / 50);
+                    this.rotateSpeedY.push(Math.random() / 50);
+                    this.rotateSpeedZ.push(Math.random() / 50);
+                    this.add(piece);
+                }
+            }
+            this.collapsing = true;
+            this.prevTimestamp = timeStamp;
+        } else {
+            const delta = (timeStamp - this.prevTimestamp) / 300;
+            for (let i = 0; i < this.pieces.length; i++) {
+                this.pieces[i].position.y -= this.fallSpeed[i] * delta;
+                this.pieces[i].rotateX(this.rotateSpeedX[i] * delta);
+                this.pieces[i].rotateY(this.rotateSpeedY[i] * delta);
+                this.pieces[i].rotateZ(this.rotateSpeedZ[i] * delta);
+                this.pieces[i].material.opacity -= 0.005 * delta;
+            }
+        }
     }
 }
 
